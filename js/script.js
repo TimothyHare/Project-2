adc0129b79d9d4e58a7beaabc55cee7a42bd48b9
TimothyHare <cordellth@yahoo.com>
2018-09-07 21:11:45 -0400

    edit 1

---
+++ js/script.js
@@ -0,0 +1,30 @@
+/******************************************
+Treehouse Techdegree:
+FSJS project 2 - List Filter and Pagination
+******************************************/
+
+// Add variables that store DOM elements you will need to reference and/or manipulate
+// Variable that holds all students name
+const studentAlias = document.getElementsByClassName('student-item cf');
+//Variable that allows only ten student to be shown on page
+const numberOfStudents = math.ceil (studentAlias.length/10);
+// Variables that recognize page length
+const page = document.querySelector(".page");
+const pageHeader = document.querySelector(".page-header");
+// Variables to create Pagination
+const newDiv = document.createElement("div");
+const a = document.createElement("a");
+
+// Create a function to hide all of the items in the list excpet for the ten you want to show
+// Tip: Keep in mind that with a list of 54 studetns, the last page will only display four
+
+
+
+
+// Create and append the pagination links - Creating a function that can do this is a good approach
+
+
+
+
+// Add functionality to the pagination buttons so that they show and hide the correct items
+// Tip: If you created a function above to show/hide list items, it could be helpful here
